c493b6a0dcc1a10006259fd1eadba77e71e0ebb0
Davronn <davronmahamadjonov8@gmai.com>
2024-07-23 22:31:05 +0500

    .

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-lg">The project is being worked on</h1>
+    </div>
+  );
+}
+
+export default Home;
